002fdd4dab798cdcc957378bb672c83ce80b16be
shreesha <shreesha.k.upadhya@gmail.com>
2024-04-30 21:40:01 +0530

    Pushing code

--- src/test/java/org/example/ProjectLoginTest1.java
+++ src/test/java/org/example/ProjectLoginTest1.java
@@ -22,85 +22,85 @@ public class ProjectLoginTest1 extends ProjectBase1
         projectLoginPage = new ProjectLoginPage1();
     }
 
-    //Register - Test Case 1
-//    @Test(priority = 0)
-//    public void siteRegister() throws InterruptedException {
-//        projectLoginPage.RegisterUser();
-//    }
+   // Register - Test Case 1
+    @Test(priority = 0)
+    public void siteRegister() throws InterruptedException {
+        projectLoginPage.RegisterUser();
+    }
 
     //Signout - Test Case 2
-//    @Test(priority = 1)
-//    public void signOut() throws InterruptedException{
-//        projectLoginPage.SignOut();
-//    }
+    @Test(priority = 1)
+    public void signOut() throws InterruptedException{
+        projectLoginPage.SignOut();
+    }
 
-    //Forgot Password - Test Case 3
-//    @Test(priority = 2)
-//    public void forgotPassword() throws InterruptedException {
-//        projectLoginPage.ForgotPass();
-//    }
+   // Forgot Password - Test Case 3
+    @Test(priority = 2)
+    public void forgotPassword() throws InterruptedException {
+        projectLoginPage.ForgotPass();
+    }
 
 
     //Login - Test Case 4
-//    @Test(priority = 3)
-//    public void siteLogin() throws InterruptedException {
-//        projectLoginPage.LoginUser();
-//    }
+    @Test(priority = 3)
+    public void siteLogin() throws InterruptedException {
+        projectLoginPage.LoginUser();
+    }
 
-    //Select Men products - Test Case 5
-//    @Test(priority = 4)
-//    public void selectMenproduct() throws InterruptedException
-//    {
-//        projectLoginPage.SelectMenproducts();
-//    }
-//
-//    //Delete Cart
-//    @Test(priority = 5)
-//    public void deleteCartitem() throws InterruptedException
-//    {
-//        projectLoginPage.DeleteCartitem();
-//    }
-//
-//    //Add to Cart - Test Case 6
-//    @Test(priority = 6)
-//    public void AddtoProduct() throws InterruptedException
-//    {
-//        projectLoginPage.AddtoCart();
-//    }
-//
-//    //Verify Cart - Test Case 7
-//    @Test(priority = 7)
-//    public void CartPage() throws InterruptedException
-//    {
-//        projectLoginPage.CartPage();
-//    }
-//
-//    //Test Case 8
-//    @Test(priority = 8)
-//    public void ProceedCheckout() throws InterruptedException
-//    {
-//        projectLoginPage.ProceedCheckout();
-//    }
-//
-//    //Test Case 9
-//    @Test(priority = 9)
-//    public void ShippingAddress()throws InterruptedException
-//    {
-//        projectLoginPage.ShippingAddress();
-//    }
-//
-//    //Test Case 10
-//    @Test(priority = 10)
-//    public void PlaceOrder() throws InterruptedException
-//    {
-//        projectLoginPage.PlaceOrder();
-//    }
-//
-//    @Test(priority = 11)
-//    public void ContinueShopping() throws InterruptedException
-//    {
-//        projectLoginPage.ContinueShopping();
-//    }
+   // Select Men products - Test Case 5
+    @Test(priority = 4)
+    public void selectMenproduct() throws InterruptedException
+    {
+        projectLoginPage.SelectMenproducts();
+    }
+
+    //Delete Cart
+    @Test(priority = 5)
+    public void deleteCartitem() throws InterruptedException
+    {
+        projectLoginPage.DeleteCartitem();
+    }
+
+    //Add to Cart - Test Case 6
+    @Test(priority = 6)
+    public void AddtoProduct() throws InterruptedException
+    {
+        projectLoginPage.AddtoCart();
+    }
+
+    //Verify Cart - Test Case 7
+    @Test(priority = 7)
+    public void CartPage() throws InterruptedException
+    {
+        projectLoginPage.CartPage();
+    }
+
+    //Test Case 8
+    @Test(priority = 8)
+    public void ProceedCheckout() throws InterruptedException
+    {
+        projectLoginPage.ProceedCheckout();
+    }
+
+    //Test Case 9
+    @Test(priority = 9)
+    public void ShippingAddress()throws InterruptedException
+    {
+        projectLoginPage.ShippingAddress();
+    }
+
+    //Test Case 10
+    @Test(priority = 10)
+    public void PlaceOrder() throws InterruptedException
+    {
+        projectLoginPage.PlaceOrder();
+    }
+
+    @Test(priority = 11)
+    public void ContinueShopping() throws InterruptedException
+    {
+        projectLoginPage.ContinueShopping();
+    }
 
     @Test(priority = 12)
     public void SigninGettitle() throws InterruptedException
@@ -108,11 +108,11 @@ public class ProjectLoginTest1 extends ProjectBase1
         projectLoginPage.SigninGettitle();
     }
 
-//    @Test(priority = 13)
-//    public void InvalidEmailerror() throws InterruptedException
-//    {
-//        projectLoginPage.InvalidEmailerror();
-//    }
+    @Test(priority = 13)
+    public void InvalidEmailerror() throws InterruptedException
+    {
+        projectLoginPage.InvalidEmailerror();
+    }
 
     @Test(priority = 14)
     public void AddtoWishlist() throws InterruptedException
@@ -187,8 +187,8 @@ public class ProjectLoginTest1 extends ProjectBase1
     }
 
     //Quit Browser - Test Case
-//    @Test(priority = 26)
-//    public void closeBrowser() throws InterruptedException{
-//        projectLoginPage.CloseBrowser();
-//    }
+    @Test(priority = 26)
+    public void closeBrowser() throws InterruptedException{
+        projectLoginPage.CloseBrowser();
+    }
 }
